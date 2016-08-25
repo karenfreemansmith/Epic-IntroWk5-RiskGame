f26b6e5bd8db6a67faea3dbb41231cd693337484
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-08-25 07:54:53 -0700

    expand object definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,42 @@
 // Backend Logic
-function Player() {}
-
+function Game() {
+  this.players=[];
+  this.dice=[];
+  this.cards=[];
+  this.objectives[];
+}
+function Objective() {
+  this.reward=new Reward("{description}");
+}
+function Reward(description) {
+  this.description=description;
+}
 function Die() {}
+function Card() {}
 
-function Continent() {}
-function Country() {}
-function Troups() {}
 
-function Game() {}
-function Objectives() {}
+function Player() {
+  this.playerColor="";
+  this.objectives=[];
+  this.territories=[];
+}
+
+function Continent(continentName) {
+  this.continentName=continentName;
+  this.territories=[];
+  this.continentColor="";
+}
+function Territory(territoryName) {
+  this.territoryName=territoryName;
+  this.city=false;
+  this.capital=false;
+  this.troups=0;
+}
+function Capital() {}
+function City() {}
+function Troup(color) {
+  this.playerColor=color;
+}
+
 
 // Frontend Logic
