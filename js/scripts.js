77f22199d22f1442b1d37d6a7e3daab56da4d46f
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-08-25 06:33:41 -0700

    initial backend logic - objects needed

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Backend Logic
+function Player() {}
+
+function Die() {}
+
+function Continent() {}
+function Country() {}
+function Troups() {}
+
+function Game() {}
+function Objectives() {}
+
+// Frontend Logic
