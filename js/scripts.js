5c2a33d31b7569bf6056c279117af343dd3f6ccd
Aimen Khakwani and Jena SanCartier <aimen@>
2016-08-29 16:49:41 -0700

    add dice to game

--- js/scripts.js
+++ js/scripts.js
@@ -78,19 +78,19 @@ function Objective() {
 function Reward(description) {
   this.description=description;
 }
-function Die(sides, type) {
-  this.sides = sides;
-  this.type = type; //defense, attack, defense bonus, attack bonus
-  this.color = ""; //determined by type, attack=black, defense=red
-  this.value = 1;
-  this.roll();
-}
-
-Die.prototype.roll=function(){
-    min = 1;
-    max = this.sides;
-    this.value = Math.floor(Math.random()*(max - min+1)) + min;
-}
+// function Die(sides, type) {
+//   this.sides = sides;
+//   this.type = type; //defense, attack, defense bonus, attack bonus
+//   this.color = ""; //determined by type, attack=black, defense=red
+//   this.value = 1;
+//   this.roll();
+// }
+//
+// Die.prototype.roll=function(){
+//     min = 1;
+//     max = this.sides;
+//     this.value = Math.floor(Math.random()*(max - min+1)) + min;
+// }
 
 function Card(name, image, stars) {
   this.territory=territoryName;
@@ -176,6 +176,41 @@ function Troup(color) {
   this.playerColor=color;
 }
 
+// Dice JS
+function Die(sides, type, color, value) {
+  this.sides = sides;
+  this.type = type; //defense, attack, defense bonus, attack bonus
+  this.color = color; //determined by type, attack=black, defense=red
+  this.value = 1;
+  this.roll();
+}
+
+Die.prototype.roll=function(){
+    min = 1;
+    max = this.sides;
+    this.value = Math.floor(Math.random()*(max - min+1)) + min;
+}
+
+$("#battle").click(function(){
+  var redOne =  new Die(6, "defense", "red");
+  var redTwo =  new Die(6, "defense", "red");
+  var blackOne =  new Die(6, "attack", "black");
+  var blackTwo =  new Die(6, "attack", "black");
+  var blackThree =  new Die(6, "attack", "black");
+  redOne.roll();
+  redTwo.roll();
+  blackOne.roll();
+  blackOne.roll();
+  blackOne.roll();
+  $(".red").show();
+  $(".black").show();
+  $("#defenseOne").text(redOne.value);
+  $("#defenseTwo").text(redTwo.value);
+  $("#attackOne").text(blackOne.value);
+  $("#attackTwo").text(blackTwo.value);
+  $("#attackThree").text(blackThree.value);
+
+})
 
 // Frontend Logic
 var newGame = new Game();
@@ -194,9 +229,13 @@ $("h4").click(function(){
   msg += "</ul>";
   msg += "<p>Troop Draft</p>";
   msg += "<p>Place Troops</p>";
-  msg += "<button>Attack</button>";
+  msg += "<button id='attack'>Attack</button>";
   msg += "<button>End Attack Manuvers</button>";
   msg += "<button>End Turn Manuvers</button>";
   msg += "<p>Draw Card</p>";
   $("#activePlayer").html(msg);
+  $("#activePlayer").toggle();
+  $("#attack").click(function(){
+    $("#battle").show();
+  });
 });
